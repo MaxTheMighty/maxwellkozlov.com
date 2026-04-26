78bad628072da4c483118038c77ac1d7ceb27f14
Maxwell Kozlov <maxwellkozlov1@gmail.com>
2026-04-26 10:38:25 -0700

    Added vite and vue

---
+++ src/components/HomePage.vue
@@ -0,0 +1,31 @@
+<template>
+<header>
+
+</header>
+    <!-- Header -->
+    <div class="flex flex-row justify-between space gap-10 p border border-gray border-dashed"> 
+        <!-- About me image and bio -->
+        <div class="flex flex-wrap p-5">
+            <img class="w-50 h-70" src="../assets/me.jpg"></img>
+            <div class="min-w-0 flex-1">
+            <p class="px-3 text-wrap">
+                My name is Maxwell and I am a Software Engineer with a passion for simulations and engines. I have professional experience with [legacy software sqlxpress stuff] and personal experience with [space simluation stuff]
+            </p>
+        </div> 
+        </div>
+
+        <!-- Links -->
+        <div class="flex flex-wrap p-5">
+            <h3>Links</h3>
+        </div>
+    </div>
+
+    <div>
+        Main content goes here
+    </div>
+</template>
+
+
+<script setup lang="ts">
+
+</script>
